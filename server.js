7087e18ac4cb3e66c37dd9534a9217df47934592
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-05-16 10:54:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,71 +13,68 @@ var sql = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
   password: "",
-  database: "javascript"
+  database: "Capchat"
 });  
 
 sql.connect();
 
 app.get('/',  function(req, res) {
-  
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-
-  res.send("ok");
-
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    res.send("ok");
 })
 
-app.get('/auteurs',  function(req, res) {
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
+// app.get('/auteurs',  function(req, res) {
+//   res.setHeader("Content-Type", "application/json; charset=utf-8");
 
-  sql.query('SELECT * from auteurs', function(err, rows) {
-    if (err) {
-      res.status(400).end(JSON.stringify(err));
-    }
-    res.send(rows).end();
-  });
+//   sql.query('SELECT * from auteurs', function(err, rows) {
+//     if (err) {
+//       res.status(400).end(JSON.stringify(err));
+//     }
+//     res.send(rows).end();
+//   });
 
-})
+// })
 
-app.get('/auteurs/:userId',  function(req, res) {
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
+// app.get('/auteurs/:userId',  function(req, res) {
+//   res.setHeader("Content-Type", "application/json; charset=utf-8");
 
-  sql.query(`SELECT * from auteurs WHERE ID = ${req.params.userId}`, function(err, rows) {
-    if (err) {
-      res.status(400).end(JSON.stringify(err));
-    }
-    res.send(rows);
-  });
+//   sql.query(`SELECT * from auteurs WHERE ID = ${req.params.userId}`, function(err, rows) {
+//     if (err) {
+//       res.status(400).end(JSON.stringify(err));
+//     }
+//     res.send(rows);
+//   });
 
-})
+// })
 
-app.post('/auteurs', function(req, res) {
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  sql.query(`INSERT INTO auteurs (Nom, Prenom) VALUES ('${req.body.Nom}','${req.body.Prenom}')`, function(err) {
-    if (err) {
-      res.status(400).end(JSON.stringify(err));
-    }
-    res.status(201).end();
-  })
-})
+// app.post('/auteurs', function(req, res) {
+//   res.setHeader("Content-Type", "application/json; charset=utf-8");
+//   sql.query(`INSERT INTO auteurs (Nom, Prenom) VALUES ('${req.body.Nom}','${req.body.Prenom}')`, function(err) {
+//     if (err) {
+//       res.status(400).end(JSON.stringify(err));
+//     }
+//     res.status(201).end();
+//   })
+// })
 
-app.put('/auteurs', function(req, res) {
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  sql.query(`UPDATE auteurs SET Nom = '${req.body.Nom}' , Prenom = '${req.body.Prenom}' WHERE Id_Auteurs = '${req.body.id}'`, function(err) {
-    if (err) {
-      res.status(400).end(JSON.stringify(err));
-    }
-    res.status(200).end();
-  })
-})
+// app.put('/auteurs', function(req, res) {
+//   res.setHeader("Content-Type", "application/json; charset=utf-8");
+//   sql.query(`UPDATE auteurs SET Nom = '${req.body.Nom}' , Prenom = '${req.body.Prenom}' WHERE Id_Auteurs = '${req.body.id}'`, function(err) {
+//     if (err) {
+//       res.status(400).end(JSON.stringify(err));
+//     }
+//     res.status(200).end();
+//   })
+// })
 
-app.delete('/auteurs', function(req, res) {
-  sql.query(`DELETE FROM auteurs WHERE Id_Auteurs = '${req.body.id}'`, function(err) {
-    if (err) {
-      res.status(400).end(JSON.stringify(err));
-    }
-    res.status(200).end();
-  })
-})
+// app.delete('/auteurs', function(req, res) {
+//   sql.query(`DELETE FROM auteurs WHERE Id_Auteurs = '${req.body.id}'`, function(err) {
+//     if (err) {
+//       res.status(400).end(JSON.stringify(err));
+//     }
+//     res.status(200).end();
+//   })
+// })
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
