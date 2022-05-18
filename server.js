2e3076d4a70f4982aab8bf2af0fbc99e1062cc05
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-05-18 23:38:25 +0200

    le login marche pas encore

--- server.js
+++ server.js
@@ -1,17 +1,50 @@
 var express = require('express');
+const jwt = require('jsonwebtoken')
 var bodyParser = require('body-parser');
 const cors = require('cors')
 const port = 3000
 var mysql = require('mysql');
 const fs = require('fs');
 const crypto = require('crypto');
+
 require('dotenv').config();
 
 if (!fs.existsSync('.env')) {
-  fs.appendFile('.env', `SECRET_TOKEN=${crypto.randomBytes(64).toString('hex')}`, function (err) {
+  fs.appendFile('.env', `SECRET_TOKEN=${crypto.randomBytes(64).toString('hex')}\nSECRET_TOKEN_MDP=${crypto.randomBytes(64).toString('hex')}`, function (err) {
     if (err) throw err;
   });
 }
+
+/* Récupération du header bearer */
+const extractBearerToken = headerValue => {
+  if (typeof headerValue !== 'string') {
+      return false
+  }
+
+  const matches = headerValue.match(/(bearer)\s+(\S+)/i)
+  return matches && matches[2]
+}
+
+/* Vérification du token */
+const checkTokenMiddleware = (req, res, next) => {
+  // Récupération du token
+  const token = req.headers.authorization && extractBearerToken(req.headers.authorization)
+
+  // Présence d'un token
+  if (!token) {
+      return res.status(401).json({ message: 'Error. Need a token' })
+  }
+
+  // Véracité du token
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, decodedToken) => {
+      if (err) {
+          res.status(401).json({ message: 'Error. Bad token' })
+      } else {
+          return decodedToken
+      }
+  })
+}
+
 
 var app = express();
 app.use(cors())
@@ -29,14 +62,74 @@ sql.connect();
 
 app.get('/',  function(req, res) {
     res.setHeader("Content-Type", "application/json; charset=utf-8");
-    res.send("ok");
+    const hash = crypto.createHmac('sha512', process.env.SECRET_TOKEN_MDP)
+    hash.update("test");
+    const test = hash.digest('hex').toString()
+    res.send(test);
 })
 
-app.get('/createSecretToken',  function(req, res) {
+app.post('/api/connexion', async function(req, res) {
   res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.send(process.env.SECRET_TOKEN);
+
+  const users = await getUsers();
+  const user = users.find(u => u.nom == req.body.username && u.mdp == hash(req.body.password));
+  if (!user) {
+    return res.status(400).json({message: `le nom d'utilisateur ou le mot de passe sont incorrect`})
+  }
+  const token = jwt.sign({username: req.body.username}, process.env.SECRET_TOKEN, { expiresIn: '24h' });
+  return res.send({token: token})
 })
 
+app.post('/api/inscription', async function(req, res) {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  const users = await getUsers();
+  const user = users.find(u => u.nom == req.body.username);
+  if (user) {
+    res.status(400).json({message: `L'utilisateur ${req.body.username} existe déjà`})
+  }
+
+  await setUser(req.body.username, req.body.password)
+  .then(
+    () => {
+      res.send({message: `Le compte ${req.body.username} à était créé avec succès`});
+    }
+  )
+  .catch(
+    err => {
+      res.status(400).json({message: `erreur : ${err}`});
+    } 
+  );
+})
+
+app.get('/api/compte', checkTokenMiddleware, function(req,res) {
+  res.send("ça marche")
+})
+
+function getUsers() {
+  return new Promise((resolve, reject) => {
+    sql.query('SELECT nom, mdp FROM user', function (err, rows) {
+      if (err) return reject(err)
+      return resolve(rows);
+    })
+  })
+}
+
+function setUser(nom, mdp) {
+  return new Promise((resolve, reject) => {
+    const mdpHash = hash(mdp)
+    
+    sql.query(`INSERT INTO user(nom, mdp) VALUES ('${nom}','${mdpHash}')`, function (err) {
+      if (err) return reject(err);
+      return resolve();
+    })
+  })
+}
+
+function hash(mdp) {
+  const hash = crypto.createHmac('sha512', process.env.SECRET_TOKEN_MDP)
+  hash.update(mdp);
+  return hash.digest('hex').toString();
+}
 
 // app.get('/auteurs',  function(req, res) {
 //   res.setHeader("Content-Type", "application/json; charset=utf-8");
