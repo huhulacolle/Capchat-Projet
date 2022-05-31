e1c3be6914476aabebfd8c7268decc342482b753
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-05-31 13:27:19 +0200

    début capchat romain

--- src/components/Capchat.vue
+++ src/components/Capchat.vue
@@ -4,6 +4,7 @@
             ici sera le menu du capchat qui permettra de récupérer le lien iframe
         </div>
         <button @click="getCapchat()">test</button>
+        <center><canvas id="canvas" width="1000" height="1000"></canvas></center>
         <div v-for="capchat in capchats" :key="capchat.ordre">
             <img style="width: 400px; height: auto" :src="'data:' + capchat.format + ';base64, ' + capchat.img" />
             <div v-if="capchat.ImageSinguliere">
@@ -33,14 +34,42 @@ export default {
                 data => {
                     console.log(data.data);
                     this.capchats = data.data;
+                    this.drawCanvas();
                 }
+                
             )
             .catch(
                 err => {
                     console.error(err);
                 }
             )
+        },
+        drawCanvas() {
+            var canvas = document.getElementById('canvas');
+            var ctx = canvas.getContext('2d');
+            var positionX = 0;
+            var positionY = 0;
+            var img;
+            for (var i = 0; i < 9; i++) {
+                img = new Image();
+                img.src = 'data:' + this.capchats[i].format + ';base64, ' + this.capchats[i].img;
+                img.onload = function () {
+                    img.style.display = 'inline';
+                };
+                if (i % 3 == 0) {
+
+                    positionX = 0;
+                    positionY += 100;
+                } else {
+                    positionX += 100;
+                }
+                // console.log(img);
+                ctx.drawImage(img, positionX, positionY, 100, 100);
+
+            }
         }
     }
 }
 </script>
+ 
+
